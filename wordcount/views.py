b9567dabf389db9970510f120cc87b55fbef252f
vedicvamsi <samvamsi55555@gmail.com>
2018-12-15 18:09:04 +0530

    word count counts the no. of words

---
+++ wordcount/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+def homepage(request):
+    return render(request, 'home.html')
+
+def count(request):
+    fulltext = request.GET['fulltext']
+    worddictionary = {}
+    words = fulltext.split()
+    counttotal = len(words)
+    for word in words:
+        if word in worddictionary:
+            worddictionary[word] += 1
+        else:
+            worddictionary[word] = 1
+
+    sortedwords = sorted(worddictionary.items(), key = operator.itemgetter(1), reverse = True)
+    return render(request, 'count.html',{'fulltext': fulltext, 'counttotal' : counttotal, 'sortedwords' : sortedwords })
+
+def about(request):
+    return render(request,'about.html')
